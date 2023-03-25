ace23c1ff25d3d01c5e4bb701b71d43751137c21
Christoph33one <cjbrowning23@hotmail.com>
2023-03-25 13:29:01 +0000

    created a timer effect the that runs once with an empty array. So the count effect is seperate

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -2,18 +2,30 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
     useEffect(() => {
         console.log('count 1 effect')
         document.title = count;
     },[count]);
     const [count2, setCount2] = useState(0);
     // using the setCount to sepperate the orginal count from the new count (count2)!
-    // remember the useEffecet will run in every render, which will slow an app down. creating a seperate count, the counts with the useEffect
+    // remember the useEffecet will run in every render, which will slow an app down. creating a seperate count or effect, the counts with the useEffect
     // will help with preformance as it will onky render single counts when needed!
     useEffect(() => {
         console.log('count 2 effect 2')
         document.title = count;
     },[count2]);
+    // creating a time effect
+    useEffect(() => {
+        console.log('count 1 effect');
+        document.title = count;
+    }, [count]);
+    useEffect(()=>{
+        console.log('Creating timer');
+        const interval = setInterval(() => {
+            setTime(time => time + 1)
+        }, 1000);
+    }, []);
     return (
     <div>
         <button onClick={
@@ -26,6 +38,7 @@ function UseEffectCounter() {
         }>
             Increament Count ({count2})
         </button>
+        <h2>Time this{time}</h2>
     </div>
   )
 }
